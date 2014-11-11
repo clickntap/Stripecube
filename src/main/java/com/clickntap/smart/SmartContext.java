15dbdfcc77fc3cd54a033cb25a31fa8dfe418b9b
tmendici <tonino.mendicino@clickntap.com>
2014-11-11 20:10:37 +0100

    1.8

--- src/main/java/com/clickntap/smart/SmartContext.java
+++ src/main/java/com/clickntap/smart/SmartContext.java
@@ -60,7 +60,7 @@ public class SmartContext extends HashMap<String, Object> implements Serializabl
 	private Exception exception;
 
 	public String getVersion() {
-		return "1.6";
+		return "1.8";
 	}
 
 	public JSONArray toJsonArray(String array) {
