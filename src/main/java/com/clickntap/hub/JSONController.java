b598a2c71173d23a57de72250197c7a8a25e2d8d
tmendici <tonino.mendicino@clickntap.com>
2014-11-12 17:53:06 +0100

    added JSON and XML controllers

---
+++ src/main/java/com/clickntap/hub/JSONController.java
@@ -0,0 +1,65 @@
+package com.clickntap.hub;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONObject;
+import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
+
+import com.clickntap.tool.jdbc.JdbcManager;
+import com.clickntap.utils.ConstUtils;
+
+public class JSONController extends MultiActionController {
+
+	private BOManager app;
+	private JdbcManager jdbcManager;
+
+	public BOManager getApp() {
+		return app;
+	}
+
+	public void setApp(BOManager app) {
+		this.app = app;
+	}
+
+	public JdbcManager getJdbcManager() {
+		return jdbcManager;
+	}
+
+	public void setJdbcManager(JdbcManager jdbcManager) {
+		this.jdbcManager = jdbcManager;
+	}
+
+	protected void handleRequest(HttpServletResponse response, Object object) {
+		response.setContentType("text/json;charset=UTF-8");
+		try {
+			byte[] bytes = object.toString().getBytes(ConstUtils.UTF_8);
+			response.setContentLength(bytes.length);
+			handleRequest(response, bytes);
+		} catch (Exception e) {
+			handleException(response, e);
+		}
+	}
+
+	protected void handleRequest(HttpServletResponse response, byte[] out) throws IOException {
+		response.getOutputStream().write(out);
+	}
+
+	protected void handleException(HttpServletResponse response, Throwable throwable) {
+		try {
+			ByteArrayOutputStream out = new ByteArrayOutputStream();
+			PrintWriter writer = new PrintWriter(out);
+			throwable.printStackTrace(writer);
+			writer.close();
+			JSONObject res = new JSONObject();
+			res.put("exception", throwable);
+			res.put("stackTrace", out.toString("UTF-8"));
+			handleRequest(response, res);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ src/main/java/com/clickntap/hub/XMLController.java
@@ -0,0 +1,69 @@
+package com.clickntap.hub;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.dom4j.io.OutputFormat;
+import org.dom4j.io.XMLWriter;
+import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
+
+import com.clickntap.tool.jdbc.JdbcManager;
+import com.clickntap.utils.PListUtils;
+
+public class XMLController extends MultiActionController {
+
+	private BOManager app;
+	private JdbcManager jdbcManager;
+
+	public BOManager getApp() {
+		return app;
+	}
+
+	public void setApp(BOManager app) {
+		this.app = app;
+	}
+
+	public JdbcManager getJdbcManager() {
+		return jdbcManager;
+	}
+
+	public void setJdbcManager(JdbcManager jdbcManager) {
+		this.jdbcManager = jdbcManager;
+	}
+
+	protected void handleRequest(HttpServletResponse response, Object object) {
+		response.setContentType("text/xml; UTF-8");
+		try {
+			OutputFormat format = OutputFormat.createCompactFormat();
+			ByteArrayOutputStream out = new ByteArrayOutputStream();
+			XMLWriter writer = new XMLWriter(out, format);
+			writer.write(PListUtils.toPList(object).getDocument());
+			response.setContentLength(out.size());
+			handleRequest(response, out.toByteArray());
+		} catch (Exception e) {
+			handleException(response, e);
+		}
+	}
+
+	protected void handleRequest(HttpServletResponse response, byte[] out) throws IOException {
+		response.getOutputStream().write(out);
+	}
+
+	protected void handleException(HttpServletResponse response, Throwable throwable) {
+		try {
+			ByteArrayOutputStream out = new ByteArrayOutputStream();
+			PrintWriter writer = new PrintWriter(out);
+			throwable.printStackTrace(writer);
+			writer.close();
+			XMLErrorResponse res = new XMLErrorResponse();
+			res.setException(throwable);
+			res.setStackTrace(out.toString("UTF-8"));
+			handleRequest(response, res);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
