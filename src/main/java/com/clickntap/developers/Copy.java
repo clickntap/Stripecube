21e05b64c2d3b91e0808c5a17fe8c21ec71321d6
Tonino Mendicino <tmendici@clickntap.com>
2017-02-20 04:52:34 +0100

    Copy main

---
+++ src/main/java/com/clickntap/developers/Copy.java
@@ -0,0 +1,12 @@
+package com.clickntap.developers;
+
+import com.clickntap.utils.Robocopy;
+
+public class Copy {
+
+	public static void main(String[] args) throws Exception {
+		args = new String[2];
+		Robocopy.main(args);
+	}
+
+}
